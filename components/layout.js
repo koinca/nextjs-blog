ee15c2704f0f016b7fe3e1b068961c92d6aafb40
“koinca” <koinca>
2021-04-23 10:23:51 -0700

    Update name.

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Zebrium'
+const name = 'Zeee'
+
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
